49986bdce28557e66ccd48eb93815a1c4297c78f
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-12-01 19:04:02 +0200

    feat: add /users

---
+++ src/data/users.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    id: '59b3b81a-32cb-409c-b21f-31b27c32d161',
+    name: 'User one',
+    login: 'User1',
+    password: '111',
+  },
+  {
+    id: '59b3b81a-32cb-409c-b21f-31b27c32d162',
+    name: 'User two',
+    login: 'User2',
+    password: '222',
+  },
+  {
+    id: '59b3b81a-32cb-409c-b21f-31b27c32d163',
+    name: 'User three',
+    login: 'User3',
+    password: '333',
+  },
+];
+
+module.exports = users;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
+const fastify = require('fastify')({ logger: true });
 const { PORT } = require('./common/config');
-const app = require('./app');
+fastify.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swager: {
+    info: { title: 'fastify-api' },
+  },
+});
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+fastify.register(require('./routes/users'));
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
