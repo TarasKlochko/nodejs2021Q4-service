43210205bc4036da3c18fc61a3aa8eeee07db912
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-12-19 12:25:24 +0200

    refactor: try fix github checking

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,10 +5,10 @@ dotenv.config({
   path: path.join(__dirname, '../../.env'),
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env['PORT'] || 5000;
 const { NODE_ENV } = process.env;
 const { MONGO_CONNECTION_STRING } = process.env;
 const { JWT_SECRET_KEY } = process.env;
-const AUTH_MODE = process.env.AUTH_MODE === 'true';
+const AUTH_MODE = process.env['AUTH_MODE'] === 'true';
 
 export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, AUTH_MODE };
