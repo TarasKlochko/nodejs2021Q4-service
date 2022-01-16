010c09de09b3129a73de6e2f245586c8ef836151
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2022-01-16 18:41:01 +0200

    feat: add POSTGRES env

--- src/common/config.ts
+++ src/common/config.ts
@@ -7,8 +7,20 @@ dotenv.config({
 
 const PORT = process.env['PORT'] || 5000;
 const { NODE_ENV } = process.env;
-const { MONGO_CONNECTION_STRING } = process.env;
 const { JWT_SECRET_KEY } = process.env;
 const AUTH_MODE = process.env['AUTH_MODE'] === 'true';
+const { POSTGRES_PORT } = process.env;
+const { POSTGRES_USER } = process.env;
+const { POSTGRES_PASSWORD } = process.env;
+const { POSTGRES_DB } = process.env;
 
-export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, AUTH_MODE };
+export {
+  PORT,
+  NODE_ENV,
+  JWT_SECRET_KEY,
+  AUTH_MODE,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+};
