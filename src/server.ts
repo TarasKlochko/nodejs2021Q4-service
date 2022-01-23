d4ae3b6b7067a5ae94755cc922ea69698d26ab75
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2022-01-23 19:07:18 +0200

    feat: add default user admin

--- src/server.ts
+++ src/server.ts
@@ -9,54 +9,70 @@ import userRoutes from './routes/users';
 import { PORT } from './common/config';
 import 'reflect-metadata';
 import { ConfigDB } from '../ormconfig';
+import { Users } from './entity/user.entity';
+import bcrypt from 'bcryptjs';
 
-createConnection(ConfigDB).then(() => {
-  const fastify: FastifyInstance<Server, IncomingMessage, ServerResponse> =
-    Fastify({
-      logger: {
-        prettyPrint: {
-          translateTime: 'SYS:standard',
-          ignore: 'pid,hostname,reqId',
+createConnection(ConfigDB).then(async (connection) => {
+  if (connection.isConnected) {
+    const isAdmin = await Users.findOne({ login: 'admin' });
+    if (!isAdmin) {
+      const user = Users.create({
+        name: 'Admin',
+        login: 'admin',
+        password: await bcrypt.hash('admin', 10),
+      });
+      await Users.save(user);
+    }
+
+    const fastify: FastifyInstance<Server, IncomingMessage, ServerResponse> =
+      Fastify({
+        logger: {
+          prettyPrint: {
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname,reqId',
+          },
+          file: './log/file.log',
         },
-        file: './log/file.log',
-      },
+      });
+
+    fastify.addHook('onSend', (req, reply, _payload, done) => {
+      req.log.info(
+        {
+          url: req.raw.url,
+          body: req.body,
+          query: req.query,
+          statusCode: reply.statusCode,
+        },
+        'received request'
+      );
+      done();
     });
 
-  fastify.addHook('onSend', (req, reply, _payload, done) => {
-    req.log.info(
-      {
-        url: req.raw.url,
-        body: req.body,
-        query: req.query,
-        statusCode: reply.statusCode,
-      },
-      'received request'
-    );
-    done();
-  });
+    fastify.register(fastifySwagger, {
+      exposeRoute: true,
+      routePrefix: '/docs',
+    });
 
-  fastify.register(fastifySwagger, {
-    exposeRoute: true,
-    routePrefix: '/docs',
-  });
+    fastify.register(loginRoutes);
+    fastify.register(userRoutes);
+    fastify.register(boardRoutes);
+    fastify.register(tasksRoutes);
 
-  fastify.register(loginRoutes);
-  fastify.register(userRoutes);
-  fastify.register(boardRoutes);
-  fastify.register(tasksRoutes);
+    /**
+     * Listens user port. If an error occurs then log one and stops server
+     * @returns void
+     */
 
-  /**
-   * Listens user port. If an error occurs then log one and stops server
-   * @returns void
-   */
-
-  const start = async () => {
-    try {
-      await fastify.listen(PORT, '0.0.0.0');
-    } catch (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-  };
-  start();
+    const start = async () => {
+      try {
+        await fastify.listen(PORT, '0.0.0.0');
+      } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+      }
+    };
+    start();
+  } else {
+    await connection.connect();
+  }
 });
